91f148b805e004c94193b1852add9d9392adf5bb
Alexey Raga <alexey.raga@gmail.com>
2017-02-03 08:31:04 +1100

    compiles

--- src/Kafka/Conduit/Sink.hs
+++ src/Kafka/Conduit/Sink.hs
@@ -6,8 +6,7 @@ module Kafka.Conduit.Sink
 
 import Java
 import Data.Conduit
-import Kafka.Producer (KafkaProducer, JFuture, JRecordMetadata)
-import qualified Kafka.Producer as KP
+import Kafka.Producer
 
 import Control.Monad.IO.Class
 import Control.Monad (void)
@@ -16,15 +15,6 @@ import Control.Monad.Trans.Resource
 import Kafka.Types as X
 import Kafka.Producer.Types as X
 import Kafka.Producer.ProducerProperties as X
-
-newProducer :: MonadIO m => ProducerProperties -> m (KafkaProducer JByteArray JByteArray)
-newProducer props = liftIO . java $ KP.newProducer props
-
-closeProducer :: MonadIO m => KafkaProducer k v -> m ()
-closeProducer c = liftIO . java $ c <.> KP.closeProducer
-
-send :: MonadIO m => KafkaProducer JByteArray JByteArray -> ProducerRecord JByteArray JByteArray -> m (JFuture JRecordMetadata)
-send p r = liftIO . java $ p <.> KP.send r
 
 kafkaSink :: MonadResource m => ProducerProperties -> Sink (ProducerRecord JByteArray JByteArray) m ()
 kafkaSink props =
--- src/Kafka/Conduit/Source.hs
+++ src/Kafka/Conduit/Source.hs
@@ -5,7 +5,7 @@ module Kafka.Conduit.Source
 
 import Java
 import Data.Conduit
-import Kafka.Consumer (ConsumerProperties, KafkaConsumer, Timeout, TopicName, ConsumerRecord)
+import Kafka.Consumer
 import qualified Kafka.Consumer as KC
 
 import Control.Monad.IO.Class
@@ -15,18 +15,6 @@ import Control.Monad.Trans.Resource
 import Kafka.Types as X
 import Kafka.Consumer.Types as X
 import Kafka.Consumer.ConsumerProperties as X
-
-newConsumer :: MonadIO m => ConsumerProperties -> m (KafkaConsumer JByteArray JByteArray)
-newConsumer props = liftIO . java $ KC.newConsumer props
-
-closeConsumer :: MonadIO m => KafkaConsumer k v -> m ()
-closeConsumer c = liftIO . java $ c <.> KC.closeConsumer
-
-poll :: MonadIO m => KafkaConsumer JByteArray JByteArray -> Timeout -> m [ConsumerRecord (Maybe JByteArray) (Maybe JByteArray)]
-poll c t = liftIO . java $ c <.> KC.poll t
-
-subscribe :: MonadIO m => KafkaConsumer JByteArray JByteArray -> [TopicName] -> m ()
-subscribe c ts = liftIO . java $ c <.> KC.subscribeTo ts
 
 kafkaSource :: (MonadResource m)
             => ConsumerProperties
@@ -38,7 +26,7 @@ kafkaSource props tm ts =
   where
     mkConsumer = do
       cons <- newConsumer props
-      subscribe cons ts
+      subscribeTo cons ts
       return cons
 
     clConsumer = closeConsumer
